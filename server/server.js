04b268e62cb3acf1cfd530bf9afbfc79aaf4332f
Timple Basinger <tbasinger@protonmail.com>
2021-11-01 16:17:06 -0500

    finish

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 let app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from the web server side...');
+app.use((req, res, next) => {
+    console.log(req.url);
+
+    next();
 });
+
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello from the web server side...');
+// });
 
 app.listen(3000);
